9cda24b006e435a84540cdd016e337dec8591b33
Jasur <fake49735@gmail.com>
2025-04-25 17:05:37 +0500

    ErrorModalIcon updated

--- vite.config.ts
+++ vite.config.ts
@@ -2,38 +2,38 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
-import viteCompression from 'vite-plugin-compression'
+import viteCompression from 'vite-plugin-compression';
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [
-		react(),
-		tailwindcss(),
-		viteCompression({ algorithm: 'brotliCompress' }),
-	],
-	build: {
-		sourcemap: true,
-		outDir: 'dist',
-		target: 'esnext',
-	},
-	resolve: {
-		alias: {
-			'@': path.resolve(__dirname, './src'),
-		},
-	},
-	preview: {
-		port: 5173,
-		strictPort: true,
-		host: '0.0.0.0',
-		allowedHosts: true,
-	},
-	server: {
-		port: 5173,
-		allowedHosts: true,
-		hmr: {
-			protocol: 'wss',
-			host: 'localhost',
-			port: 5173,
-		},
-	},
+  plugins: [
+    react(),
+    tailwindcss(),
+    viteCompression({ algorithm: 'brotliCompress' })
+  ],
+  build: {
+    sourcemap: true,
+    outDir: 'dist',
+    target: 'esnext',
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  preview: {
+    port: 5173,
+    strictPort: true,
+    host: '0.0.0.0',
+    allowedHosts: true,
+  },
+  server: {
+    port: 5173,
+    allowedHosts: true,
+    hmr: {
+      protocol: 'wss',
+      host: 'localhost',
+      port: 5173,
+    },
+  },
 })
