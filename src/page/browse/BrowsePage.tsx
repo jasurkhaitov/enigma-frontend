90150ac94d89cd0001981c7fa193e181ebc100bd
Jasur <fake49735@gmail.com>
2025-05-12 13:45:47 +0500

    container built

--- src/page/browse/BrowsePage.tsx
+++ src/page/browse/BrowsePage.tsx
@@ -15,11 +15,9 @@ export default function BrowsePage() {
 				<BrowseHeader />
 			</section>
 
-			<section id="dashboard" className='py-5'>
-				
-			</section>
+			<section id="dashboard" className='py-5'></section>
 
-			<section id="about-us" className='w-full bg-[linear-gradient(96.43deg,_#575DEB_12.29%,_#313585_104.34%)]'>
+			<section id="about-us" className='w-full m-auto bg-[linear-gradient(96.43deg,_#575DEB_12.29%,_#313585_104.34%)] rounded-md'>
 				<div className='max-w-[1440px] w-full px-4 sm:px-6 lg:px-8 m-auto py-12 sm:py-16 lg:py-24'>
 					<BrowseMain />
 				</div>
