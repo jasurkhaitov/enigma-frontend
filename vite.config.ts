15b829e2c56af434d64eb418fedc5bd38cb6dc7f
Jasur <fake49735@gmail.com>
2025-05-12 14:34:36 +0500

    allowed this host, add "enigmadoc.com" to server.allowedHosts in vite.config.js.

--- vite.config.ts
+++ vite.config.ts
@@ -2,39 +2,39 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
-import viteCompression from 'vite-plugin-compression';
+import viteCompression from 'vite-plugin-compression'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    viteCompression({ algorithm: 'brotliCompress' })
-  ],
-  build: {
-    sourcemap: true,
-    outDir: 'dist',
-    target: 'esnext',
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-  preview: {
-    port: 5173,
-    strictPort: true,
-    host: '0.0.0.0',
-    allowedHosts: ['enigmadoc.com'],
-  },  
-  server: {
-    port: 5173,
-    host: true,
-    hmr: {
-      protocol: 'ws',
-      host: 'localhost',
-      port: 5173,
-    },
-  },
-  
+	plugins: [
+		react(),
+		tailwindcss(),
+		viteCompression({ algorithm: 'brotliCompress' }),
+	],
+	build: {
+		sourcemap: true,
+		outDir: 'dist',
+		target: 'esnext',
+	},
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+		},
+	},
+	preview: {
+		port: 5173,
+		strictPort: true,
+		host: '0.0.0.0',
+		allowedHosts: ['enigmadoc.com'],
+	},
+	server: {
+		port: 5173,
+		host: true,
+		allowedHosts: ['enigmadoc.com'],
+		hmr: {
+			protocol: 'ws',
+			host: 'localhost',
+			port: 5173,
+		},
+	},
 })
